f21a8033d96ceede396d6b1b84ee08db42c8d60e
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-07-07 12:39:45 -0500

    ejercicios 13 14 y 15

---
+++ ejercicios1314y15/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TaskFormik from '../src/components/pure/form/taskFormik'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TaskFormik></TaskFormik>
+      </header>
+    </div>
+  );
+}
+
+export default App;
