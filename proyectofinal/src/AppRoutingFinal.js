6b2a65d159a7b276e8781000ecacab85e680cf3c
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-07-10 08:55:41 -0500

    Iniciando Material Ui en en Dashboard

--- proyectofinal/src/AppRoutingFinal.js
+++ proyectofinal/src/AppRoutingFinal.js
@@ -1,14 +1,40 @@
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import LoginPage from './pages/auth/LoginPage'
 import Notfoundpage from './pages/404/NotFoundPage'
+import DashBoardPage from './pages/dashboard/DashBoardPage';
 
 
 function AppRoutingFinal() {
+
+  //TODO Change to value false session storage
+  let loggedIn = true;
 
   return (
     <Router>
       {/* Route Switch */}
       <Switch>
-        <Route component={Notfoundpage}></Route>
+      {/* Redirections to protect our routes */}
+      <Route exact path='/'>
+        {
+          loggedIn ? 
+          <Redirect from='/' to='/dashboard' />
+          :
+          <Redirect from='/' to='/login'/>
+        }
+      </Route>
+      {/* Dashboard Routes */}
+      <Route exact path='/dashboard'>
+        {
+          loggedIn ? 
+          <DashBoardPage />
+          :
+          <Redirect from='/' to='/login'/>
+        }
+      </Route>
+      {/* Login Route */}
+      <Route exact path='/login' component={LoginPage} />
+      <Route exact path='/dashboard' component={DashBoardPage}/>
+      <Route component={Notfoundpage}></Route>
       </Switch>
     </Router>
       
--- proyectofinal/src/pages/auth/LoginPage.jsx
+++ proyectofinal/src/pages/auth/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Loginformik from '../../components/pure/forms/loginFormik'
 
-export default function loginPage() {
+export default function LoginPage() {
   return (
     <div>
         <h1>Login Page</h1>
